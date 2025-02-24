85a62be08fc077699de5fb0c9e6579d27dddde40
Blinde16 <blakejlinde@gmail.com>
2025-02-24 15:08:52 -0700

    Added migration

--- Models/TaskContext.cs
+++ Models/TaskContext.cs
@@ -1,6 +1,15 @@
-﻿namespace mission8Assignment.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mission8Assignment.Models
 {
-    public class TaskContext : TaskRepository
+    public class TaskContext : DbContext
     {
+
+        public TaskContext(DbContextOptions<TaskContext> options) : base (options) 
+        { 
+        
+        }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
