29323eefadf972a8260b2d607924ca7174354d0e
gabe <gabrielliza012@gmail.com>
2025-02-24 14:11:01 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mission8Assignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
