e50358fa7e27424822f0a2135275a7caba6dd89c
Blinde16 <blakejlinde@gmail.com>
2025-02-26 20:11:25 -0700

    blake Changes

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace mission8Assignment.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
